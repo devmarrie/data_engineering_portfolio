9475c3ad189de555e3ef4ea8376dfd651a8ebaf5
devmarrie <annemarrieakeyo42@gmail.com>
2023-12-22 16:00:24 +0300

    Updates

---
+++ precache-manifest.194be12de3b6a40c37800d3945a0cfd4.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8255e2b33ff931464f6c47baa2ca9688",
+    "url": "/data_engineering_portfolio/index.html"
+  },
+  {
+    "revision": "3ba5d04895b5339979c3",
+    "url": "/data_engineering_portfolio/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "4fad1e304c2ee485d0d7",
+    "url": "/data_engineering_portfolio/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "9f020bfb44cb28217ce5",
+    "url": "/data_engineering_portfolio/static/css/main.fe880c7c.chunk.css"
+  },
+  {
+    "revision": "09d4b221286fa3004a83",
+    "url": "/data_engineering_portfolio/static/js/2.44b7c856.chunk.js"
+  },
+  {
+    "revision": "800c02977d76d137ca141bc95a19c674",
+    "url": "/data_engineering_portfolio/static/js/2.44b7c856.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ba5d04895b5339979c3",
+    "url": "/data_engineering_portfolio/static/js/3.08df2204.chunk.js"
+  },
+  {
+    "revision": "4fad1e304c2ee485d0d7",
+    "url": "/data_engineering_portfolio/static/js/4.84a58414.chunk.js"
+  },
+  {
+    "revision": "9f020bfb44cb28217ce5",
+    "url": "/data_engineering_portfolio/static/js/main.502f5ffd.chunk.js"
+  },
+  {
+    "revision": "0b48568617f275da0b29",
+    "url": "/data_engineering_portfolio/static/js/runtime-main.a26bfbc8.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/data_engineering_portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "d38a06be295aab3c83c2a9611eb17c6c",
+    "url": "/data_engineering_portfolio/static/media/HackerRank_logo.d38a06be.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/data_engineering_portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "749203eb426c132a297a555dd0a81051",
+    "url": "/data_engineering_portfolio/static/media/alx.749203eb.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/data_engineering_portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "53b2ad717fb383dc83d8ec5662e35fa7",
+    "url": "/data_engineering_portfolio/static/media/cpLogo.53b2ad71.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/data_engineering_portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "35ef5e915631be2c31ccdff55b53db29",
+    "url": "/data_engineering_portfolio/static/media/harvardLogo.35ef5e91.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/data_engineering_portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "dfac4416cbc9cdd16695f1452652ee98",
+    "url": "/data_engineering_portfolio/static/media/nyc_taxi_project.dfac4416.png"
+  },
+  {
+    "revision": "4ab9b7c6375f837eb9169dd92e1028dd",
+    "url": "/data_engineering_portfolio/static/media/nytaxi_dash.4ab9b7c6.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/data_engineering_portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "3b758ef56d398145542e137daa28e392",
+    "url": "/data_engineering_portfolio/static/media/stanfordLogo.3b758ef5.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/data_engineering_portfolio/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "53015aad4cf940e4d341c556ac2d8014",
+    "url": "/data_engineering_portfolio/static/media/vodafoneLogo.53015aad.png"
+  },
+  {
+    "revision": "d2102b6757a5a746802740bc49c96f60",
+    "url": "/data_engineering_portfolio/static/media/youtube_analysis.drawio.d2102b67.svg"
+  },
+  {
+    "revision": "8e0a79eda19bdd82762907c069d24947",
+    "url": "/data_engineering_portfolio/static/media/yt_dashboard.8e0a79ed.png"
+  }
+]);
